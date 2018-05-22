f138aff4ee07c4943766bc4fb6f370b54f4e69bc
jayvib <jayson.vibandor@gmail.com>
2018-05-22 21:58:14 +0800

    added template design pattern

---
+++ behavioral/template/transaction/transaction.go
@@ -0,0 +1,62 @@
+package transaction
+
+import (
+	"strings"
+	"fmt"
+)
+
+type MessageRetriever interface {
+	 Message() string
+}
+
+type Template interface {
+	first() string
+	third() string
+	ExecuteAlgorithm(MessageRetriever) string
+}
+
+type TemplateImpl struct{}
+
+func (t *TemplateImpl) first() string {
+	return "hello"
+}
+
+func (t *TemplateImpl) third() string {
+	return "template"
+}
+
+func (t *TemplateImpl) ExecuteAlgorithm(m MessageRetriever) string {
+	return strings.Join([]string{t.first(), m.Message(), t.third()}, " ")
+}
+
+type TestStruct struct {
+	Template
+}
+
+func (m *TestStruct) Message() string {
+	return "world"
+}
+
+type AnonymousTemplate struct {}
+
+func (a *AnonymousTemplate) first() string {
+	return "hello"
+}
+
+func (a *AnonymousTemplate) third() string {
+	return "template"
+}
+
+func (a *AnonymousTemplate) ExecuteAlgorithm(m MessageRetriever) string {
+	return fmt.Sprintf("%s %s %s", a.first(), m.Message(), a.third())
+}
+
+type TemplateAdapterFunc func() string
+
+func (t TemplateAdapterFunc) Message() string {
+	return t()
+}
+
+func NewMessageRetrieverAdapter(f func() string) MessageRetriever {
+	return TemplateAdapterFunc(f)
+}
---
+++ behavioral/template/transaction/transaction_test.go
@@ -0,0 +1,38 @@
+package transaction
+
+import (
+	"testing"
+	"strings"
+)
+
+func TestTemplate_ExecuteAlgorithm(t *testing.T) {
+	t.Run("Using interface", func(t *testing.T){
+		s := &TestStruct{}
+		s.Template = &TemplateImpl{}
+		res := s.ExecuteAlgorithm(s)
+		expected := "world"
+
+		if !strings.Contains(res, expected) {
+			t.Errorf("Expected string '%s' wasn't found on returned string\n", expected)
+		}
+	})
+
+	t.Run("Using First Class function adapter to an interface", func(t *testing.T){
+		s := &TestStruct{}
+		msgRetreiver := NewMessageRetrieverAdapter(func() string { // using adapter pattern
+			return "world"
+		})
+
+		if msgRetreiver == nil {
+			t.Fatal("Message Retriever must not be nill it will panic ahead")
+		}
+
+		s.Template = &AnonymousTemplate{}
+		res := s.ExecuteAlgorithm(msgRetreiver)
+		expected := "world"
+
+		if !strings.Contains(res, expected) {
+			t.Errorf("Expected string '%s' wasn't found on returned string\n", expected)
+		}
+	})
+}
