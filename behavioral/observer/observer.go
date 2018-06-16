50c3f367e42449972539d21bf9cc7be2f81ab91f
Jayson Vibandor <jaysonv@isell.local>
2018-06-16 22:38:42 +0800

    added observer pattern

---
+++ behavioral/observer/observer.go
@@ -0,0 +1,18 @@
+package observer
+
+type Observer interface {
+	Notify(message string)
+}
+
+type Publisher struct {
+	ObserversList []Observer
+}
+
+func (s *Publisher) AddObserver(o Observer) {}
+
+func (s *Publisher) RemoveObserver(o Observer) {}
+
+func (s *Publisher) NotifyObservers(m string) {}
+
+
+
