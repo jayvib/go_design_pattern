ecf98714d375976a6faff7b69f6cef5062746ea0
Jayson Vibandor <jayson.vibandor@gmail.com>
2019-08-10 10:11:40 +0800

    Review Files for Template pattern
    Added review files for the template design pattern

---
+++ behavioral/template/review/string-processor/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"strings"
+
+	"github.com/sirupsen/logrus"
+)
+
+type processorFunc func(string) string
+
+type template struct {
+	msg string
+}
+
+func (t template) addprefix(str string) string {
+	return "-" + str
+}
+
+func (t template) addsuffix(str string) string {
+	return str + "-"
+}
+
+func (t template) Execute(fn processorFunc) string {
+	return t.addsuffix(fn(t.addprefix(t.msg)))
+
+}
+
+func main() {
+	t := template{msg: "Hello World"}
+	res := t.Execute(func(str string) string {
+		return strings.ToLower(str)
+	})
+	logrus.Println(res)
+}
